89ccb12b96ef0477d7cad431aa4753590deea50c
Elias da Rosa <elias@eliasdarosa.com.br>
2010-01-02 14:26:37 -0200

    JOOMLA - Criado script para download/upload do banco de dados do servidor

---
+++ db/config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+
+# Dados de conexão com o servidor remoto
+BANC_SERVER=""
+HOST_SERVER=""
+USER_SERVER=""
+PASS_SERVER=""
+
+
+# Dados de conexão com o servidor local
+BANC_LOCAL=""
+HOST_LOCAL=""
+USER_LOCAL=""
+PASS_LOCAL=""
+
+
+# Pastas
+MYSQL_DIR="/d/SITES/xampp/mysql/bin"
+
+
+# nome do arquivo
+FILE_NAME="$BANC_SERVER.sql"
---
+++ db/download.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+
+# carrega o arquivo de configuração
+souce db/config.sh
+
+
+# Mensagem de inicio
+echo ""
+echo -e "\033[33;1m Script iniciado \033[0m"
+
+
+# Exporta o banco de dados
+echo "  - Exportando o banco de dados do servidor $HOST_SERVER"
+$MYSQL_DIR/mysqldump.exe --default-character-set=utf8 --host=$HOST_SERVER --user=$USER_SERVER --password=$PASS_SERVER $BANC_SERVER > $FILE_NAME
+
+
+# Apaga todas as tabelas do banco
+echo "  - Excluindo todos as tabelas do servidor $HOST_LOCAL"
+DROP=$($MYSQL_DIR/mysql.exe --host=$HOST_LOCAL --user=$USER_LOCAL --password=$PASS_LOCAL $BANC_LOCAL -e "SELECT concat('DROP TABLE',' ',table_name,';') as TABELAS FROM information_schema.TABLES WHERE TABLE_SCHEMA='$BANC_LOCAL';" | sed s/TABELAS// )
+$MYSQL_DIR/mysql.exe --host=$HOST_LOCAL --user=$USER_LOCAL --password=$PASS_LOCAL $BANC_LOCAL -e "$DROP"
+
+
+# Importando o novo banco de dados 
+echo "  - Importando o novo banco de dados no servidor $HOST_LOCAL"
+$MYSQL_DIR/mysql.exe --host=$HOST_LOCAL --user=$USER_LOCAL --password=$PASS_LOCAL $BANC_LOCAL < $FILE_NAME
+
+
+# Mensagem de Finalização
+echo ""
+echo -e "\033[33;1m Script finalizado \033[0m"
