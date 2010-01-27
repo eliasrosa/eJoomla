635b062313475c1cc509911694dc261833f16121
Elias da Rosa <elias@eliasdarosa.com.br>
2010-01-27 17:34:08 -0200

    JOOMLA - Adicionado jQuery 1.4.1 no eLoad

--- sql/joomla - full.sql
+++ sql/joomla - full.sql
@@ -1532,3 +1532,36 @@ ALTER TABLE `versao_db` CHANGE `requerido_0003` `requerido_0004` BIT(1) NULL DEF
 ALTER TABLE `jos_ecomp_routers_rules`  ADD `idcomponente` INT(11) NOT NULL AFTER `itemid`;
 
 
+
+
+
+
+-- ----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------
+-- REQUERIDO 0004
+
+ALTER TABLE `versao_db` CHANGE `requerido_0004` `requerido_0005` BIT(1) NULL DEFAULT NULL;
+
+UPDATE `jos_plugins` SET `params` = 'jqueryAdd=1
+jqueryServer=localhost
+jqueryVersion=1.4.1
+removeMootools=1
+removeMootoolsUncompressed=1
+removeCaption=1
+generator=1
+titulo=1
+tituloSite={sitename} - {title}
+eImageResizeAll=2
+eImageResizeClass=eImageResize
+eImageNoResizeClass=eImageNoResize
+lightBox-active=1
+lightBox-overlayOpacity=0.8
+lightBox-txtImage=Imagem
+lightBox-txtOf=de
+firebug-adicionar=0
+analytics-adicionar=1
+analytics-UA=' WHERE `jos_plugins`.`id` = 36 LIMIT 1;
+
+
