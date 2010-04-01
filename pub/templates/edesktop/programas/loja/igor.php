83970b21fdaacc80585f9b77be5070ef56b6ae50
Elias da Rosa <elias@eliasdarosa.com.br>
2010-04-01 12:08:24 -0300

    JOOMLA - Removido/Alterado arquivos teste do programa "loja"

--- pub/templates/edesktop/programas/loja/igor.php
+++
@@ -1,5 +0,0 @@
-igor
-
-<a href="loja/index" class="link">loja -> self</a><br>
-
-<? echo JRequest::getvar('id', 0); ?>
--- pub/templates/edesktop/programas/loja/index.php
+++ pub/templates/edesktop/programas/loja/index.php
@@ -1,4 +1,1 @@
-teste<br><br>
-<br>
-<a href="loja/elias" class="link">elias -> self</a><br>
-<a href="loja/igor?id=ok_99999" class="link" target="new">elias -> novo</a>
+<h1>Loja Virtual</h1>
