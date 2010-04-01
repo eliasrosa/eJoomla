1d2f047db64c4ea27b3b3e798c159845a7f017c4
Elias da Rosa <elias@eliasdarosa.com.br>
2010-04-01 12:10:35 -0300

    JOOMLA - Adicionado uma função js teste para confirmações

--- pub/templates/edesktop/js/eDesktop.functions.js
+++ pub/templates/edesktop/js/eDesktop.functions.js
@@ -16,6 +16,31 @@ $(function(){
 			}
 		});
 		
-	};	
+	};
+	
+	window.confirm_test = function(msg){
+		
+		var bool = '';
+		var html = '<div id="dialog-confirm" title="Atenção"><p>'+ msg +'</p></div>';
+		
+		$(html).dialog({
+			closeOnEscape: false,
+			modal: true,
+			buttons: {
+				Cancelar : function() {
+					$(this).dialog('close');
+					$(this).dialog('destroy');
+					return false;
+				},
+				OK : function(a) {
+					$(this).dialog('close');
+					$(this).dialog('destroy');
+					return true;
+				}
+			}
+		});
+	
+		
+	};		
 
 });
