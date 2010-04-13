94d05451514dc9988f9bc3e23fa7a4e796f59062
Elias da Rosa <elias@eliasdarosa.com.br>
2010-04-13 05:14:54 -0300

    JOOMLA - Corrigido o remover usuarios, o sistema este alinha com as tabelas core do joomla

--- pub/templates/edesktop/programas/usuarios/paginas/usuarios.remover.php
+++ pub/templates/edesktop/programas/usuarios/paginas/usuarios.remover.php
@@ -1,16 +1,28 @@
 <?
 $ids = JRequest::getvar('ids');
 
+if(count($ids) == 0)
+	jexit(json_encode(array('tipo' => 'error', 'msg' => 'Nenhum usuário foi selecionado!', 'retorno' => false)));	
 
 foreach($ids as $id)
 {
-	$u = new JCRUD("jos_users");
+	$u = new JCRUD('jos_users');
 	$u = $u->busca_por_id($id);
 	$u->delete();
+	
+	$aro = new JCRUD("jos_core_acl_aro");
+	$aro = $aro->busca_por_id($u->id, 'value');
+	$aro->delete();
+	
+	$groups_aro_map = new JCRUD("jos_core_acl_groups_aro_map");
+	$groups_aro_map = $groups_aro_map->busca_por_id($aro->id, 'aro_id');
+	$groups_aro_map->delete($aro->id, 'aro_id');
 }
 
 if(count($ids) == 1)
-	echo "{'msg' : 'Usuário removido com sucesso!' }";
+	$r = array('msg' => 'Usuário removido com sucesso!', 'retorno' => true);
 else
-	echo "{'msg' : 'Usuários removidos com sucesso!' }";
+	$r = array('msg' => 'Usuários removidos com sucesso!', 'retorno' => true);
+	
+jexit(json_encode($r));
 ?>
