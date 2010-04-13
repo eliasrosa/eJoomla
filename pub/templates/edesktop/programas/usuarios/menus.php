119b876556cc907a85acfd4aff069f328e3f0653
Elias da Rosa <elias@eliasdarosa.com.br>
2010-04-13 07:50:39 -0300

    JOOMLA - Alterado o texto do menu principal

--- pub/templates/edesktop/programas/usuarios/menus.php
+++ pub/templates/edesktop/programas/usuarios/menus.php
@@ -1,7 +1,7 @@
 <?
 	$menu_principal = new menu_lateral('Menu principal');
 	$menu_principal->add('Listar todos usuários', 'usuarios.lista');	
-	$menu_principal->add('Adicionar novo usuário', 'usuarios.form');
+	$menu_principal->add('Cadastro de usuário', 'usuarios.form');
 	$menu_principal->add('Administrar grupos de usuários', 'grupos.lista');
 	
 	$menu_grupos = new menu_lateral('Administrar grupos de usuários', true);
