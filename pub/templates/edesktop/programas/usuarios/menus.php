6fdd74bc92da4ff29103cb9a75a83a5db9657c52
Elias da Rosa <elias@eliasdarosa.com.br>
2010-04-13 05:03:06 -0300

    JOOMLA - Alterado o menu do programa usuários

--- pub/templates/edesktop/programas/usuarios/menus.php
+++ pub/templates/edesktop/programas/usuarios/menus.php
@@ -1,10 +1,10 @@
 <?
 	$menu_principal = new menu_lateral('Menu principal');
 	$menu_principal->add('Listar todos usuários', 'usuarios.lista');	
-	$menu_principal->add('Adicionar novo usuário', 'usuarios.add');
+	$menu_principal->add('Adicionar novo usuário', 'usuarios.form');
 	$menu_principal->add('Administrar grupos de usuários', 'grupos.lista');
 	
 	$menu_grupos = new menu_lateral('Administrar grupos de usuários', true);
 	$menu_grupos->add('Listar todos grupos', 'grupos.lista');	
-	$menu_grupos->add('Adicionar novo grupo', 'grupos.add');
+	$menu_grupos->add('Adicionar novo grupo', 'grupos.form');
 ?>
