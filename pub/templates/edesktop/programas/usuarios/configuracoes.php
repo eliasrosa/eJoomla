2bce6a2b43af215dce6b2492acf1262fccb760c4
Elias da Rosa <elias@eliasdarosa.com.br>
2010-04-16 10:55:38 -0300

    JOOMLA - Adicionado algumas permissões no programa usuários

--- pub/templates/edesktop/programas/usuarios/configuracoes.php
+++ pub/templates/edesktop/programas/usuarios/configuracoes.php
@@ -3,9 +3,19 @@ $configuracoes = array
 (
     'titulo' => 'Usuários',
     'finder' => true,
-    'default' => 'usuarios.lista'
-);
+    'default' => 'usuarios.lista',
 
-//Lista de usuários cadastrados no sistema
-//Cadastre novo usuário para acessar o sistema
+	'permissoes' => array(
+		'usuarios.lista' => 'Visualizar lista de usuários',
+		'usuarios.form' => 'Visualizar dados dos usuários',
+		'usuarios.editar' => 'Editar dados dos usuários',
+		'usuarios.adicionar' => 'Adicionar novos usuários',
+		'usuarios.remover' => 'Remover usuários',
+		'grupos.lista' => 'Visualizar lista de grupos de usuários',
+		'grupos.form' => 'Visualizar dados dos grupos de usuários',
+		'grupos.editar' => 'Editar dados dos grupos de usuários',
+		'grupos.adicionar' => 'Adicionar novos grupos de usuários',
+		'grupos.remover' => 'Remover grupos de usuários'
+	)
+);
 ?>
