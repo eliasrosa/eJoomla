83a1fde332c7e5a1d7062bc513744850f9591874
Elias da Rosa <elias@eliasdarosa.com.br>
2010-04-19 14:59:50 -0300

    JOOMLA - Corrigido o redirecionamento da pasta adm

--- pub/adm/index.php
+++ pub/adm/index.php
@@ -1,1 +1,10 @@
-<?php header("Location: ../?template=edesktop"); ?>
+<?
+define( '_JEXEC', 1 );
+define( 'DS', DIRECTORY_SEPARATOR );
+define('JPATH_BASE', dirname(__FILE__) .DS. '..' );
+
+require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
+require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
+
+header('Location: ' .JURI::base(1). '/../?template=edesktop');
+?>
