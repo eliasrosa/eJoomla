63e1fca69d78b0950fffe20143e85d881b62cf25
Elias da Rosa <elias@eliasdarosa.com.br>
2010-04-23 01:10:57 -0300

    JOOMLA - Removido o style-ie.css e o comum.js do template novidades-2010

--- pub/templates/novidades-2010/index.php
+++ pub/templates/novidades-2010/index.php
@@ -1,20 +1,12 @@
 <?php
-
-	header("X-UA-Compatible: IE=EmulateIE7");
-	$url = "{$this->baseurl}/templates/{$this->template}";
-
-	//print_r($this);
-	
+    header("X-UA-Compatible: IE=EmulateIE7");
+    $url = "{$this->baseurl}/templates/{$this->template}";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
 	<jdoc:include type="head" />
 	<link type="text/css" href="<?= $url ?>/styles/<?= $this->params->get('tpl_style'); ?>/style.css" rel="Stylesheet" />
-	<script type="text/javascript" src="<?= $url ?>/js/comum.js"></script>
-	<!--[if lte IE 7]>
-	    <link type="text/css" href="<?= $url ?>/css/style-ie.css" rel="Stylesheet" />
-	<![endif]-->
     </head>
     <body>
 	<div id="corpo">
