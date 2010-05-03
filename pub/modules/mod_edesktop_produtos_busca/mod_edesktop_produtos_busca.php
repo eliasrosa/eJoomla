20dc7ed8ff3beb37112f43cc102262240d0a185f
Elias da Rosa <elias@eliasdarosa.com.br>
2010-05-03 02:22:28 -0300

    JOOMLA - O valor da busca será exibido no input do mod

--- pub/modules/mod_edesktop_produtos_busca/mod_edesktop_produtos_busca.php
+++ pub/modules/mod_edesktop_produtos_busca/mod_edesktop_produtos_busca.php
@@ -5,12 +5,15 @@ defined('_JEXEC') or die( "Acesso Restrito" );
 $mid = $params->get('mod_id');
 $tit = $params->get('mod_titulo');
 $iid = $params->get('mod_itemid');
+
+// carrega o busca
+$busca = JRequest::getvar('q', '');
 ?>
 
 <div id="<?= $mid ?>" class="modEdesktopProdutosBusca">
 	<form method="get" action="<?= JRoute::_("index.php?option=com_edesktop&view=loja&layout=busca&Itemid={$iid}"); ?>">
 		<?= $module->showtitle ? "<h2>$tit</h2>" : ""; ?>
-		<input name="q" value="" type="input" class="text" />
+		<input name="q" value="<?= $busca; ?>" type="input" class="text" />
 		<input value="Buscar" type="submit" class="submit" />
 	</form>
 </div>
