5ce9f122695ea4e4828da22c855d1e7fb3707cd5
Elias da Rosa <elias@eliasdarosa.com.br>
2010-05-04 15:20:20 -0300

    JOOMLA - Adicionado funções nos biblioteca de fabricantes

--- pub/libraries/edesktop/programas/produtos/fabricantes.php
+++ pub/libraries/edesktop/programas/produtos/fabricantes.php
@@ -31,5 +31,36 @@ class edesktop_produtos_fabricantes
 		$r = $this->db->busca_por_id($id);
 		return $r;
 	}
+	
+	
+	/* busca todos os fabricantes
+	 ***************************************************/
+	function busca_todos($limit = '')
+	{
+		$limit = empty($limit) ? '' : "LIMIT 0,{$limit}";
+		$dados = $this->db->busca("WHERE status = '1' $limit");
+		
+		foreach($dados as $f)
+			$f->imagem = $this->caminho_imagem($f->id);
+		
+		return $dados;
+	}
+	
+
+	/* retorna com o caminho da imagem
+	 ***************************************************/
+	private function caminho_imagem($id)
+	{
+		$img = "/media/com_edesktop/loja/imagens/fabricantes/{$id}.jpg";
+		$url = JURI::base(1). $img;
+				
+		if(!file_exists(JPATH_BASE. $img))
+			$url = JURI::base(1). "/media/com_edesktop/loja/imagens/404.jpg";
+		
+		// retorno os dados 
+		return $url;
+	}
+	
+	
 }
 ?>
