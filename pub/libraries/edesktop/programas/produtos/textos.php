6a12e43ccf3f4ee8b8723ac6b8bdcf25a726b718
Elias da Rosa <elias@eliasdarosa.com.br>
2010-05-11 15:13:21 -0300

    JOOMLA - Adicionado biblioteca de textos dos produtos

---
+++ pub/libraries/edesktop/programas/produtos/textos.php
@@ -0,0 +1,56 @@
+<?
+// carrega o jcrud
+jimport('edesktop.jcrud');
+
+// inicia a class
+class edesktop_produtos_textos
+{
+	/* Object JCRUD
+	 ***************************************************/
+	public $db;
+
+
+
+	/* nome da tabela de textos
+	 ***************************************************/
+	private $tabela = 'jos_edesktop_produtos_textos';
+	
+
+
+	/* Inicia a class
+	 ***************************************************/
+	function __construct()
+	{
+		// Abre a tabela
+		$this->db = new JCRUD($this->tabela);
+	}
+	
+	
+	
+	
+	/* busca um texto pelo id
+	 ***************************************************/
+	function busca_por_id($id)
+	{
+		// textos
+		$dados = $this->db->busca_por_id($id);
+				
+		// retorno os dados 
+		return $dados;
+	}
+	
+	
+	
+	
+	/* busca todos os textos pelo id do produto
+	 ***************************************************/
+	function busca_por_produto($id)
+	{
+		// textos
+		$dados = $this->db->busca("WHERE idproduto = '{$id}' AND status = '1' ORDER BY ordem ASC");
+						
+		// retorno dos dados 
+		return $dados;
+	}
+}
+?>
