0e3a0233fb63cf75658e5e0a7e1db034daf7c853
Elias da Rosa <elias@eliasdarosa.com.br>
2010-05-20 08:38:35 -0300

    JOOMLA - Adicionado mod carrinho da loja

---
+++ pub/modules/mod_edesktop_loja_carrinho/mod_edesktop_loja_carrinho.php
@@ -0,0 +1,41 @@
+<?php
+defined('_JEXEC') or die( "Acesso Restrito" );
+
+
+$mid = $params->get('mod_id');
+$tit = $params->get('mod_titulo');
+$iid = $params->get('mod_itemid');
+
+
+// adiciona o style css da loja
+JHTML::stylesheet('style.css', 'media/com_edesktop/loja/css/');
+
+// busca dados
+$itens = @$_SESSION['loja.carrinho.itens'];
+$dados = @$_SESSION['loja.carrinho.dados'];
+
+// link do carrinho
+$linkCarrinho = JRoute::_("index.php?option=com_edesktop&view=loja&layout=carrinho&Itemid={$iid}")
+?>
+
+<div id="<?= $mid ?>" class="modEdesktopLojaCarrinho">
+	<?= $module->showtitle ? "<h2>$tit</h2>" : ""; ?>
+	<div class="corpo">
+		
+		<? if(count($itens)): ?>
+			<? foreach($itens as $i): ?>
+			<a class="itens" href="<?= $linkCarrinho; ?>" title="<?= $i['nome']; ?>">
+				<?= $i['nome']; ?> <span class="valor">R$ <?= number_format($i['valor'], 2, ',', '.'); ?></span>		
+			</a>
+			<? endforeach; ?>
+			<div class="subtotal">
+				<span class="txt">subtotal:</span>
+				<span class="valor">RS <?= number_format($dados['subtotal'], 2, ',', '.'); ?></span>
+				<br class="clearfix" />
+			</div>
+		<? else: ?>
+			<p class="vazio">Nenhum item foi adicionado no carrinho</p>
+		<? endif; ?>
+		<a class="visualizar" title="Visualizar carrinho" href="<?= $linkCarrinho; ?>">Visualizar carrinho</a>		
+	</div>
+</div>
