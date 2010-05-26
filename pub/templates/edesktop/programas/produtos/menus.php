daf7dad60405e0fa795893f130dd8a76f679ea96
Elias da Rosa <elias@eliasdarosa.com.br>
2010-05-26 17:39:33 -0300

    JOOMLA - Renomeado o arquivo de cadastro de produtos

--- pub/templates/edesktop/programas/produtos/menus.php
+++ pub/templates/edesktop/programas/produtos/menus.php
@@ -1,5 +1,5 @@
 <?
 	$menu_principal = new menu_lateral('Menu principal');
 	$menu_principal->add('Listar todos produtos', 'produtos.lista');	
-	$menu_principal->add('Cadastro de produto', 'produtos.novo');
+	$menu_principal->add('Cadastro de produto', 'produtos.form');
 
