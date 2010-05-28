0ec13afbefe9dae30f7bd843e1056e4f44657f3d
Elias da Rosa <elias@eliasdarosa.com.br>
2010-05-28 14:11:45 -0300

    JOOMLA - Adicionado campo opcões na tabela de produtos

--- sql/joomla - full.sql
+++ sql/joomla - full.sql
@@ -1911,3 +1911,21 @@ UPDATE `jos_edesktop_loja_configuracoes` SET `value` = '', `comentario` = 'E-mai
 
 
 
+-- ----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------
+-- REQUERIDO 0013
+
+ALTER TABLE `versao_db` CHANGE `requerido_0013` `requerido_0014` BIT(1) NULL DEFAULT NULL;
+
+-- adiciona opcoes
+ALTER TABLE `jos_edesktop_produtos_produtos`  ADD `opcoes` LONGTEXT NOT NULL AFTER `referencia`;
+
+
+
+
+
+
+
+
