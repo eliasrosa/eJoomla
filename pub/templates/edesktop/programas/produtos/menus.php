096c561df14d6d87d4419049d65491dfed77f63a
Elias da Rosa <elias@eliasdarosa.com.br>
2010-06-02 12:18:12 -0300

    JOOMLA - Adicionado menu de cadastro de categorias

--- pub/templates/edesktop/programas/produtos/menus.php
+++ pub/templates/edesktop/programas/produtos/menus.php
@@ -2,5 +2,6 @@
 	$menu_principal = new menu_lateral('Menu principal');
 	$menu_principal->add('Listar todos produtos', 'produtos.lista');	
 	$menu_principal->add('Cadastro de produto', 'produtos.form');
-	$menu_principal->add('Categorias', 'categorias.lista');
+	$menu_principal->add('Listar todas categorias', 'categorias.lista');
+	$menu_principal->add('Cadastro de categoria', 'categorias.form');
 
