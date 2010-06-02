25f41ba461d17aba0f30549e69f1a12cc0dba1da
Elias da Rosa <elias@eliasdarosa.com.br>
2010-06-02 12:19:57 -0300

    JOOMLA - Removido o remover do form e adicionado na listagem

---
+++ pub/templates/edesktop/programas/produtos/paginas/categorias.remover.php
@@ -0,0 +1,31 @@
+<?
+$ids = JRequest::getvar('ids');
+
+if(count($ids) == 0)
+	jexit(json_encode(array('tipo' => 'error', 'msg' => 'Nenhuma categoria foi selecionado!', 'retorno' => false)));	
+
+
+foreach($ids as $id)
+{
+	jimport('edesktop.programas.produtos.categorias');
+
+	$p = new edesktop_produtos_categorias();
+	
+	$filhos = $p->busca_por_idpai($id);
+	
+	foreach($filhos as $f)
+	{
+		$f->status = 0;		
+		$f->update();
+	}
+	
+	$p = $p->delete($id);
+}
+
+if(count($ids) == 1)
+	$r = array('msg' => 'Categoria removida com sucesso!', 'retorno' => true);
+else
+	$r = array('msg' => 'Categorias removidas com sucesso!', 'retorno' => true);
+	
+jexit(json_encode($r));
+?>
