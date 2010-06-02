756cb98069d91582d4b07f22887a762143e81310
Elias da Rosa <elias@eliasdarosa.com.br>
2010-06-02 00:18:33 -0300

    JOOMLA - [SQL 0014] - Adicionado campo ordem nas categorias de produtos

--- sql/joomla - full.sql
+++ sql/joomla - full.sql
@@ -1929,3 +1929,15 @@ ALTER TABLE `jos_edesktop_produtos_produtos`  ADD `opcoes` LONGTEXT NOT NULL AFT
 
 
 
+-- ----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------
+-- REQUERIDO 0014
+
+ALTER TABLE `versao_db` CHANGE `requerido_0014` `requerido_0015` BIT(1) NULL DEFAULT NULL;
+
+-- adiciona ordem nas categorias
+ALTER TABLE `jos_edesktop_produtos_categorias`  ADD `ordem` int(11) NOT NULL AFTER `status`;
+
+
