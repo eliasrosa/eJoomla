e837c974a7b03bb08b8d08adcb300bbd58c07c45
Elias da Rosa <elias@eliasdarosa.com.br>
2010-06-15 14:41:29 -0300

    JOOMLA - Adicionado varias atualizações na biblioteca de imagens

--- pub/libraries/edesktop/programas/produtos/imagens.php
+++ pub/libraries/edesktop/programas/produtos/imagens.php
@@ -15,16 +15,19 @@ class edesktop_produtos_imagens
 	 ***************************************************/
 	private $tabela = 'jos_edesktop_produtos_imagens';
 	
+	
+	/* nome da tabela de imagems
+	 ***************************************************/
+	public $pasta = "/media/com_edesktop/produtos/imagens/produtos";
 
 
 	/* Inicia a class
 	 ***************************************************/
-	function __construct()
+	function __construct($dados = array())
 	{
 		// Abre a tabela
-		$this->db = new JCRUD($this->tabela);
+		$this->db = new JCRUD($this->tabela, $dados);
 	}
-	
 	
 	
 	
@@ -34,21 +37,48 @@ class edesktop_produtos_imagens
 	{
 		// imagem
 		$dados = $this->db->busca_por_id($id);
-				
+		
+		// imagem
+		$dados->url = $this->caminho_imagem($id);
+
 		// retorno os dados 
 		return $dados;
 	}
 	
+
+	/* apaga uma imagem pelo id
+	 ***************************************************/
+	function delete($id)
+	{
+		$url = $this->caminho_imagem($id, false);
+		if($url)
+		{
+			$url = JPATH_BASE . str_replace('/ejoomla', '', $url);
+			
+			if(file_exists($url))
+				unlink($url);				
+		}
+
+		// remove do banco
+		$dados = $this->db->delete($id);
+		
+		// retorno os dados 
+		return $dados;
+	}
+
 	
 	
 	
 	/* busca todas as imagens ignorando em destaque
 	 * pelo id do produto
 	 ***************************************************/
-	function busca_por_produto($id)
+	function busca_por_produto($id, $todas = false)
 	{
+		// busca tudo
+		$where = $todas ? "": "AND destaque != '1' AND status = '1'";
+		
 		// imagem
-		$dados = $this->db->busca("WHERE idproduto = '{$id}' AND destaque != '1' AND status = '1'");
+		$dados = $this->db->busca("WHERE idproduto = '{$id}' {$where}");
 		
 		foreach($dados as $d)
 			$d->url = $this->caminho_imagem($d->id);
@@ -81,14 +111,19 @@ class edesktop_produtos_imagens
 
 	/* retorna com o caminho da imagem
 	 ***************************************************/
-	private function caminho_imagem($id)
+	private function caminho_imagem($id, $img404 = true)
 	{
-		$img = "/media/com_edesktop/produtos/imagens/produtos/{$id}.jpg";
+		$img = $this->pasta . "/{$id}.jpg";
 		$url = JURI::base(1). $img;
 		
 		if(!file_exists(JPATH_BASE. $img))
-			$url = JURI::base(1). "/media/com_edesktop/imagens/404.jpg";
-		
+		{
+			if($img404)
+				$url = JURI::base(1). "/media/com_edesktop/imagens/404.jpg";
+			else
+				$url = false;
+		}
+			
 		// retorno os dados 
 		return $url;
 	}
