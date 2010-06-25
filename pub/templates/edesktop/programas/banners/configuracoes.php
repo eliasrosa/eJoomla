d33a56de190c467734e1db3ca27bfd238fbb7dda
Elias da Rosa <elias@eliasdarosa.com.br>
2010-06-25 00:43:35 -0300

    JOOMLA - Adicinado as configurações do programa banners

--- pub/templates/edesktop/programas/banners/configuracoes.php
+++ pub/templates/edesktop/programas/banners/configuracoes.php
@@ -1,7 +1,31 @@
 <?
-$configuracoes = array
-(
-    'titulo'  => 'Banners',
-    'finder'  => false,   
+$configuracoes = array (
+        // Titulo do programa
+        'titulo' => 'Banners',
+
+        // Liberar buscas rápidas
+        'finder' => false,
+
+        // Página padrão
+        'default' => 'banners.lista',
+
+        // Permissões de acesso
+        'permissoes' => array(
+        
+			// banners
+			'banners.lista' => 'Visualizar lista de banners',
+			'banners.form' => 'Visualizar dados dos banners',
+			'banners.editar' => 'Editar dados dos banners',
+			'banners.adicionar' => 'Adicionar novos banners',
+			'banners.remover' => 'Remover banners',
+			'banners.admin' => 'Adiministrar uploads'
+
+        
+        ),
+
+        // Permissões de acesso liberadas
+        'permissoes.liberadas' => array(
+			'banners.salvar'
+        )
 );
 ?>
