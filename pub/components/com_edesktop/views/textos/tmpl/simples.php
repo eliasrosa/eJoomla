1f52259d734f549ec3e6585630046317b565c287
Elias da Rosa <elias@eliasdarosa.com.br>
2010-07-05 14:48:41 -0300

    JOOMLA - Finalizado o view textos simples

---
+++ pub/components/com_edesktop/views/textos/tmpl/simples.php
@@ -0,0 +1,6 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+require($this->template .DS. $this->params->get('arquivo') .'.php');
+
+?>
--- pub/components/com_edesktop/views/textos/view.html.php
+++ pub/components/com_edesktop/views/textos/view.html.php
@@ -8,7 +8,27 @@ jimport( 'joomla.application.component.view');
 class edesktopVIEWtextos extends JView
 {
     function display($tpl = null)
-    { 
+    {
+		// carega o menu		
+		$menu =& JSite::getMenu();
+		
+		// carrega parâmetros do menu
+		$menuItem =& $menu->getActive();	
+		$this->assignRef('menu', $menuItem);
+		
+		// carrega parâmetros
+		$params = $menu->getParams($menuItem->id);		
+		$this->assignRef('params', $params);
+
+
+		$app =& JFactory::getApplication();
+		$templateDir = JPATH_THEMES .DS. $app->getTemplate();
+		
+		
+		
+		
+		$this->assignRef('template', $templateDir);
+	
         parent::display($tpl);
     }
 }
