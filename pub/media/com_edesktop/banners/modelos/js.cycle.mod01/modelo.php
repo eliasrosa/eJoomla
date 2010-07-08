4032c1d9237be808a68ef37fffa485cc2f7b6fd5
Elias da Rosa <elias@eliasdarosa.com.br>
2010-07-08 18:46:14 -0300

    JOOMLA - Adicionado background no modelo 01 dos banners

--- pub/media/com_edesktop/banners/modelos/js.cycle.mod01/modelo.php
+++ pub/media/com_edesktop/banners/modelos/js.cycle.mod01/modelo.php
@@ -8,7 +8,7 @@ foreach($slides as $s)
 	$link = $s->url ? JRoute::_($s->url) : 'javascript:void(0);';
 	$target = $s->target ? ' target="' .$s->target. '"' : '';
 
-	echo "<a href=\"{$link}\"{$target}><img src=\"{$s->arquivo}\" width=\"{$banner->largura}\" height=\"{$banner->altura}\"/></a>";
+	echo "<a href=\"{$link}\"{$target} style=\"background: url([eimage src='{$s->arquivo}' width='{$banner->largura}' height='{$banner->altura}' fit='outside']) center center; width:{$banner->largura}px; height:{$banner->altura}px; display: block;\"></a>";
 }
 echo '</div>';
 
