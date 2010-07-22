64334ca87ffc5743ace84607c25aeed3c9146c44
Elias da Rosa <elias@eliasdarosa.com.br>
2010-07-22 16:25:23 -0300

    JOOMLA - Adicionado imagens nas listagem do finder e produtos

--- pub/templates/edesktop/programas/produtos/paginas/finder.php
+++ pub/templates/edesktop/programas/produtos/paginas/finder.php
@@ -6,7 +6,7 @@ $this->smarty->assign('finder', $finder);
 
 // produtos
 $produtos = new edProdutos();
-$produtos_dados = $produtos->busca_produtos_por_texto($finder, "AND status >= 0 ORDER BY nome ASC");
+$produtos_dados = $produtos->busca_produtos_por_texto($finder, "AND status >= 0 ORDER BY nome ASC", array('imagem'));
 $this->smarty->assign('produtos', $produtos_dados);
 $this->smarty->assign('paginacao', $produtos->paginacao);
 ?>
--- pub/templates/edesktop/programas/produtos/paginas/produtos.lista.php
+++ pub/templates/edesktop/programas/produtos/paginas/produtos.lista.php
@@ -4,9 +4,7 @@ $menu_principal->show();
 jimport('edesktop.programas.produtos');
 $edProdutos = new edProdutos();
 
-
-
-$produtos = $edProdutos->busca_todos_produtos("WHERE status >= 0 ORDER BY nome ASC");
+$produtos = $edProdutos->busca_todos_produtos("WHERE status >= 0 ORDER BY nome ASC", array('imagem'));
 
 $this->smarty->assign('produtos', $produtos);
 $this->smarty->assign('paginacao', $edProdutos->paginacao);
