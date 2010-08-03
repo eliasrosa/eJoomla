1ee61b02c3ebbd7e484fd891178f82e5ec3f9923
Elias da Rosa <elias@eliasdarosa.com.br>
2010-08-03 20:21:50 -0300

    JOOMLA - Bloqueado o acesso ao sistema para todos os navegadores exceto o navegador Firefox

--- pub/templates/edesktop/index.php
+++ pub/templates/edesktop/index.php
@@ -1,6 +1,13 @@
 <?
+$b = get_browser(null, true);
+if($b['browser'] != 'Firefox')
+{
+	header ('Content-type: text/html; charset=utf-8');
+
+	$erroBrowserHtml = '<p class="aviso">Por questões de segurança e desempenho, este programa requer a utilização do navegador Mozilla Firefox.</p><p><a href="http://br.mozdev.org/download/">Download Mozilla Firefox</a></p>';	
+	jexit($erroBrowserHtml);
+}
+
 require_once('inc/edesktop.class.php');
-
 $edek = new eDesktop($this);
-
 ?>
