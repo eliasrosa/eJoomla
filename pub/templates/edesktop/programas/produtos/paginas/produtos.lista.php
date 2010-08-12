5c538997ae921208b42d4a312aa9e1d1f4de6e01
Elias da Rosa <elias@eliasdarosa.com.br>
2010-08-12 17:01:36 -0300

    JOOMLA - Refeita a lista de produtos no adm

--- pub/templates/edesktop/programas/produtos/paginas/produtos.lista.php
+++ pub/templates/edesktop/programas/produtos/paginas/produtos.lista.php
@@ -2,10 +2,43 @@
 $menu_principal->show();
 
 jimport('edesktop.programas.produtos');
-$edProdutos = new edProdutos();
 
-$produtos = $edProdutos->busca_todos_produtos("WHERE status >= 0 ORDER BY nome ASC", array('imagem'));
+$dados = edProdutos::getInstance()
+			->busca_todos_produtos()
+			->andWhere('p.status != ?', -1)
+			->createPager(null, 'edesktop');
 
-$this->smarty->assign('produtos', $produtos);
-$this->smarty->assign('paginacao', $edProdutos->paginacao);
 ?>
+<h1>Lista de produtos</h1>
+<p class="fl">Edite e remova produtos do sistema.</p>
+
+<a href="javascript:void(0);" class="add link button fr" rel="'pagina':'produtos.form'">Adicionar novo</a>
+
+<form rel="<?= $this->processID ?>" method="post" action="">
+	<table class="ui-corner-top">
+		<tr class="ui-widget-header">
+			<td></td>
+			<td style="width: 65px;">Imagem</td>
+			<td>Nome</td>
+			<td class="tac">Status</td>
+		</tr>
+
+	<? foreach($dados->pagerLayout->execute() as $p): ?>
+		<tr class="ui-widget-content">
+			<td><input type="checkbox" name="ids[]" value="<?= $p->id ?>" /></td>
+			<td class="tac"><img src="<?= $p->imagem->url ?>" width="65" height="65" /></td>
+			<td><strong><?= $p->nome ?></strong>
+				<div class="actions">
+					<span class="editar"><a class="link" rel="'pagina': 'produtos.form', 'query': 'id=<?= $p->id ?>'" href="javascript:void(<?= $p->id ?>);">Editar</a></span>
+					<span class="excluir"><a href="javascript:void(<?= $p->id ?>);">Excluír</a></span>
+					<span class="imagens"><a class="link" rel="'pagina':'imagens.lista', 'programa': 'produtos', 'processID': 'new', 'query': 'idproduto=<?= $p->id ?>'" href="javascript:void(<?= $p->id ?>);">Imagens</a></span>
+					<span class="textos"><a class="link"  rel="'pagina':'textos.lista', 'programa': 'produtos', 'processID': 'new', 'query': 'idproduto=<?= $p->id ?>'" href="javascript:void(<?= $p->id ?>);">Textos</a></span>
+				</div>
+			</td>
+			<td class="tac"><?= $dados->getImgStatus($p->status); ?></td>
+		</tr>
+	<? endforeach; ?>
+	</table>
+	<p class="fr"><?= $dados->pagerLayout->display() ?></p>
+	<p class="excluir fl"><a href="javascript:void(0);">Excluír selecionados</a></p>
+</form>
